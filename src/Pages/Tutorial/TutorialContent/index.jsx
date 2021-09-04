bf0fa2d7273e183455538b9de5154f7d87b829cf
Albert Álef <albertalef@protonmail.com>
2021-09-04 02:52:20 -0300

    Removing ContentWrapper, he was desnecessary

--- src/Pages/Tutorial/TutorialContent/index.jsx
+++ src/Pages/Tutorial/TutorialContent/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { ContentTemplate } from '../../../Layout';
 import Welcome from './Welcome';
 import InputLink from './InputLink';
@@ -8,20 +7,9 @@ import HowUse from './HowUse';
 export default function TutorialContent() {
 	return (
 		<ContentTemplate>
-			<ContentWrapper>
-				<Welcome />
-				<InputLink />
-				<HowUse />
-			</ContentWrapper>
+			<Welcome />
+			<InputLink />
+			<HowUse />
 		</ContentTemplate>
 	);
 }
-
-export const ContentWrapper = styled.div`
-	/* background-color: #ff000014; */
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
-`;
