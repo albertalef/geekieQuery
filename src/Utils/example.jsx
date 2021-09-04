c030368bc842da6e7933e4a9c5386d405f5a9474
Albert Álef <albertalef@protonmail.com>
2021-09-04 13:27:13 -0300

    Creating example archive

---
+++ src/Utils/example.jsx
@@ -0,0 +1,159 @@
+export const responseExample = {
+	example: true,
+	questions: [
+		{
+			title: '',
+			content:
+				'<p><img src="//d379jyt2pkzgv9.cloudfront.net/item-bank/e9db620bb9771be58ba63db9563e93bf.jpg" /></p>\r\n',
+			wording:
+				'<p>O humor da tira decorre da rea&ccedil;&atilde;o de uma das cobras com rela&ccedil;&atilde;o ao uso de pronome pessoal reto, em vez de pronome obl&iacute;quo.</p>\r\n',
+			choices: [
+				{
+					content:
+						'<p>contraria o uso previsto para o registro oral da l&iacute;ngua.</p>\r\n',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>contraria a marca&ccedil;&atilde;o das fun&ccedil;&otilde;es sint&aacute;ticas de sujeito e objeto.</p>\r\n',
+					isCorrect: true,
+				},
+				{
+					content:
+						'<p>gera inadequa&ccedil;&atilde;o na concord&acirc;ncia com o verbo.</p>\r\n',
+					isCorrect: false,
+				},
+				{
+					content: '<p>gera ambiguidade na leitura do texto.</p>\r\n',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>apresenta dupla marca&ccedil;&atilde;o de sujeito.</p>\r\n',
+					isCorrect: false,
+				},
+			],
+		},
+		{
+			title: '',
+			content:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolores rerum, unde, maxime tempore odio facilis laborum nulla illo voluptatibus, minima necessitatibus quia vero rem consectetur obcaecati! Quae, commodi et?',
+			wording:
+				'<p>Assinale a alternativa em que houve erro no emprego do pronome, consistente na troca de <strong>o</strong> por <strong>lhe</strong> ou de <strong>eu</strong> por <strong>mim</strong>, ou vice-versa.</p>',
+			choices: [
+				{
+					content: '<p>Quantos livros você trouxe para eu ler?</p>',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>Eu o respeito e muito o estimo, mas não lhe obedeço cegamente.</p>',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>Essas dimensões eram sempre desgastantes para mim, que verdadeiramente o admirava.</p>',
+					isCorrect: false,
+				},
+				{ content: '<p>Foi penoso, para mim, chegar.</p>', isCorrect: false },
+				{
+					content:
+						'<p>Eu lhe convidei para a festa e não o perdoo por você ter faltado.</p>',
+					isCorrect: true,
+				},
+			],
+		},
+		{
+			title: '',
+			content:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolores rerum, unde, maxime tempore odio facilis laborum nulla illo voluptatibus, minima necessitatibus quia vero rem consectetur obcaecati! Quae, commodi et?',
+			wording:
+				'<p>Observe: “O diretor perguntou: — Onde estão os estagiários? Mandaram-<strong>nos</strong> sair? Estão no andar de cima?”. O pronome destacado pertence</p>',
+			choices: [
+				{ content: '<p>à terceira pessoa do plural.</p>', isCorrect: true },
+				{ content: '<p>à segunda pessoa do singular.</p>', isCorrect: false },
+				{ content: '<p>à terceira pessoa do singular.</p>', isCorrect: false },
+				{ content: '<p>à primeira pessoa do plural.</p>', isCorrect: false },
+				{ content: '<p>à segunda pessoa do plural.</p>', isCorrect: false },
+			],
+		},
+		{
+			title: '',
+			content:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolores rerum, unde, maxime tempore odio facilis laborum nulla illo voluptatibus, minima necessitatibus quia vero rem consectetur obcaecati! Quae, commodi et?',
+			wording:
+				'<p>Ciência e desenvolvimento tecnológico não se comportam como mercadorias: uma observação mais cuidadosa revela que tanto</p>',
+			choices: [
+				{ content: '<p>este – aquela</p>', isCorrect: true },
+				{ content: '<p>essa – aquele</p>', isCorrect: false },
+				{ content: '<p>aquele – essa</p>', isCorrect: false },
+				{ content: '<p>esta – esse</p>', isCorrect: false },
+				{ content: '<p>esse – esta</p>', isCorrect: false },
+			],
+		},
+		{
+			title: null,
+			content:
+				'<p style="text-align: center;"><img src="//res.cloudinary.com/geekie/image/upload/v1/organizations/agpzfmdlZWtpZWlkchkLEgxPcmdhbml6YXRpb24YgICAgMC1uwkM/02_fvubgu.png" /></p>',
+			wording:
+				'<p>Observando a tirinha acima, o que motivou o apito do juiz foi</p>',
+			choices: [
+				{
+					content:
+						'<p>a necessidade de empregar a ênclise para seguir a norma-padrão.</p>',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>o uso de um objeto direto no lugar de um objeto indireto.</p>',
+					isCorrect: true,
+				},
+				{
+					content:
+						'<p>a opção pelo pronome pessoal oblíquo <strong>o</strong> em vez de <strong>a</strong>.</p>',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>a obrigatoriedade da mesóclise nessa construção linguística.</p>',
+					isCorrect: false,
+				},
+				{
+					content:
+						'<p>a transgressão às regras de concordância nominal relacionadas ao pronome.</p>',
+					isCorrect: false,
+				},
+			],
+		},
+		{
+			title: '',
+			content:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolores rerum, unde, maxime tempore odio facilis laborum nulla illo voluptatibus, minima necessitatibus quia vero rem consectetur obcaecati! Quae, commodi et?',
+			wording:
+				'<p>Considerando as regras de uso dos pronomes “o” e “lhe” – e de suas formas flexionadas –, o único enunciado próprio da variedade padrão é</p>',
+			choices: [
+				{
+					content:
+						'<p>"Quando lhe encontrar, avise que estou à sua procura."</p>',
+					isCorrect: false,
+				},
+				{
+					content: '<p>"Preciso dizer a ela o quanto lhe adoro."</p>',
+					isCorrect: false,
+				},
+				{
+					content: '<p>"Tudo o que lhe disse era sincero."</p>',
+					isCorrect: true,
+				},
+				{
+					content: '<p>"Eu lhe conheço há muitos anos."</p>',
+					isCorrect: false,
+				},
+				{
+					content: '<p>"As lembranças da infância lhe perseguem."</p>',
+					isCorrect: false,
+				},
+			],
+		},
+	],
+};
