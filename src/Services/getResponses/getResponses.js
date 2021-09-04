ddbdfd694eb9cbd1567bab049b9f17b5fbd22616
Albert Álef <albertalef@protonmail.com>
2021-09-04 17:46:59 -0300

    Tranforming getResponses in a Promise

--- src/Services/getResponses/getResponses.js
+++ src/Services/getResponses/getResponses.js
@@ -2,20 +2,22 @@ import { clearQuestions } from './Cleaners';
 import { convertStringToDOM } from './Converters/convertStringToDOM';
 import { getActivity, getMainScript, getName, getQuestions } from './Getters';
 function getResponses(data, e) {
-	try {
-		const geekieDOM = convertStringToDOM(data);
-		const mainScript = getMainScript(geekieDOM);
-		const activity = getActivity(mainScript);
-		const name = getName(activity);
-		const questions = getQuestions(activity);
-		const cleanedQuestions = clearQuestions(questions);
-		return [{ name, questions: cleanedQuestions }, null];
-	} catch (err) {
-		e.preventDefault();
-		e.stopPropagation();
-		alert(err.message);
-		return [null, err];
-	}
+	return new Promise((resolve, reject) => {
+		try {
+			const geekieDOM = convertStringToDOM(data);
+			const mainScript = getMainScript(geekieDOM);
+			const activity = getActivity(mainScript);
+			const name = getName(activity);
+			const questions = getQuestions(activity);
+			const cleanedQuestions = clearQuestions(questions);
+			resolve({ name, questions: cleanedQuestions });
+		} catch (err) {
+			e.preventDefault();
+			e.stopPropagation();
+			alert(err.message);
+			reject(err);
+		}
+	});
 }
 
 export default getResponses;
